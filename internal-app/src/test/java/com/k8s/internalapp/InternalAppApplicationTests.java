fe1c04a4af581703e15ed165e912c6596fd4a890
Claudiomar Araujo <claudiomar.development@gmail.com>
2024-01-01 10:12:31 -0800

    feat: internal app with greetings api

---
+++ internal-app/src/test/java/com/k8s/internalapp/InternalAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.internalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
